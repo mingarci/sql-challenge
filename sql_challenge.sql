dcecad91ee609e2de713b914b6b27163c6f51689
Mindy Polanco <mindy@Mindys-MacBook-Pro.local>
2022-02-19 23:03:47 -0600

    Create sql_challenge.sql

---
+++ sql_challenge.sql
@@ -0,0 +1,31 @@
+
+--*perform inner join
+--*List the following details of each employee: 
+--*employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, last_name, first_name, sex, salary
+FROM employees 
+INNER JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+
+--* List first name, last name, and hire date 
+--*for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date >= '1986-01-01'
+             AND hire_date < '1987-01-01';
+
+--*List the manager of each department with the following 
+--*information: department number, department name, 
+--*the manager's employee number, last name, first name.
+
+SELECT dept_manager.dept_no, 
+dept_name, 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name 
+FROM employees 
+INNER JOIN dept_manager
+ON employees.emp_no = dept_manager.emp_no
+INNER JOIN departments
+ON dept_manager.dept_no = departments.dept_no
