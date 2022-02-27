c37db2fca8aa8389f7aba3e6feb117baceb9c7e0
Mindy Polanco <96347335+MGPolanco@users.noreply.github.com>
2022-02-26 19:28:24 -0600

    name_count

--- sql_challenge.sql
+++ sql_challenge.sql
@@ -78,5 +78,17 @@ INNER JOIN dept_emp
 INNER JOIN departments
     ON dept_emp.dept_no = departments.dept_no
 Where dept_name = 'Sales' 
-OR dept_name = 'Development'
+OR dept_name = 'Development';
+
+--* 8. In descending order, 
+--* list the frequency count of employee last names,
+--*  i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)AS Frequency
+FROM employees
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
+
+
 
